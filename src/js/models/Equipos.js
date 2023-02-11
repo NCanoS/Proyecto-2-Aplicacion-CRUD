5bf6ac1164abe40984fe22fd7305b292b391cdea
Nathaniel <nathacasa07@gmail.com>
2023-02-11 03:03:28 -0600

    creando clase equipos

---
+++ src/js/models/Equipos.js
@@ -0,0 +1,17 @@
+class Equipos {
+    nombre ="";
+    marca = "";
+    ubicacion = "";
+    cantidad = 0;
+    comentarios = "";
+    operacional = false;
+
+    constructor(nombre, marca, ubicacion, cantidad, comentarios, operacional){
+        this.nombre=nombre;
+        this.marca=marca;
+        this.ubicacion=ubicacion;
+        this.cantidad=cantidad;
+        this.comentarios=comentarios;
+        this.operacional=operacional;
+    }
+}
